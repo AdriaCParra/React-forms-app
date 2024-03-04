e72fac5acafdf289c29d5ca38820f57878bd667b
AdriaCParra <Costaadria524@gmail.com>
2024-03-04 17:26:15 +0100

    Add pipeline.yaml and update package.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
+import "./hooks";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
