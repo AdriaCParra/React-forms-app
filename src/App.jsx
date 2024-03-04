bb1cf73cb65709690152dee17fd6e5dd5744dfcf
AdriaCParra <Costaadria524@gmail.com>
2024-03-05 00:50:16 +0100

    Add search functionality to phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import "./hooks";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filteredPersons, setFilteredPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -14,6 +19,19 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleNameSearch = (event) => {
+    const searchValue = event.target.value;
+    setSearch(searchValue);
+
+    const filteredPersons = persons.filter((person) =>
+      person.name.includes(search)
+    );
+
+    setFilteredPersons(filteredPersons);
+
+    if (search === "") setFilteredPersons([]);
   };
 
   const handleSubmit = (event) => {
@@ -24,9 +42,13 @@ const App = () => {
     else setPersons([...persons, { name: newName, number: newNumber }]);
   };
 
+  const personsToShow = filteredPersons.length > 0 ? filteredPersons : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={search} onChange={handleNameSearch} />
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -38,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <li key={person.name}>{`${person.name} || ${person.number}`}</li>
         ))}
       </ul>
